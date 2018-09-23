8ca16da0fddaf6ab679e4ca3f62b91104661226f
Adam Duff <j.adam.duff@gmail.com>
2018-09-23 00:48:09 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,16 @@
 class StudentsController < ApplicationController
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    if @student.save
+      redirect_to student_path(@student)
+    end
+  end
+
 end
