8b2f3a42ebe025a1426badfa34f327b1fc3a461d
Adam Duff <j.adam.duff@gmail.com>
2018-09-23 00:44:45 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -6,8 +6,8 @@ class SchoolClassesController < ApplicationController
 
   def create
     @school_class = SchoolClass.new
-    @schoo_class.title = params[:school_class][:title]
-    @school_class.room_number = params[:school_class][:room_number]
+    @schoo_class.title = params[:title]
+    @school_class.room_number = params[:room_number]
     if @school_class.save
       redirect_to school_class_path(@school_class)
     end
